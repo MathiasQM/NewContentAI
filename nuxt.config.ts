7242218d5e4b6cbedcaf4220f1a89ace09029c12
MathiasQM <77293663+MathiasQM@users.noreply.github.com>
2024-06-12 10:56:41 +0200

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  telemetry: false,
+  css: ["~/assets/styles/tailwind.css"],
+  modules: ["nuxt-icons", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  plugins: [
+    { src: "~/plugins/firebase.ts", ssr: false },
+    { src: "~/plugins/Quill.client.ts", ssr: false },
+  ],
+});
