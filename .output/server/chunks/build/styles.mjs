4d6890ef7be97955d37c75c05298074b49c4f04a
MathiasQM <77293663+MathiasQM@users.noreply.github.com>
2024-06-12 12:21:54 +0200

    Deployment commands

--- .output/server/chunks/build/styles.mjs
+++ .output/server/chunks/build/styles.mjs
@@ -1,12 +1,12 @@
 const interopDefault = r => r.default || r || [];
 const styles = {
   "node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.B90OXfRv.mjs').then(interopDefault),
+  "node_modules/nuxt-icons/dist/runtime/components/nuxt-icon.vue": () => import('./nuxt-icon-styles.BZ7X1GZi.mjs').then(interopDefault),
+  "node_modules/nuxt-icons/dist/runtime/components/nuxt-icon.vue?vue&type=style&index=0&lang.css": () => import('./nuxt-icon-styles.ByRsLgGN.mjs').then(interopDefault),
   "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.WpyeoFtc.mjs').then(interopDefault),
   "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.BFuVBFDJ.mjs').then(interopDefault),
   "node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=922baad2&lang.css": () => import('./error-404-styles.CibK9Qn2.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=1e3620c9&lang.css": () => import('./error-500-styles.JhxDD0mW.mjs').then(interopDefault),
-  "node_modules/nuxt-icons/dist/runtime/components/nuxt-icon.vue": () => import('./nuxt-icon-styles.BZ7X1GZi.mjs').then(interopDefault),
-  "node_modules/nuxt-icons/dist/runtime/components/nuxt-icon.vue?vue&type=style&index=0&lang.css": () => import('./nuxt-icon-styles.ByRsLgGN.mjs').then(interopDefault)
+  "node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=1e3620c9&lang.css": () => import('./error-500-styles.JhxDD0mW.mjs').then(interopDefault)
 };
 
 export { styles as default };
